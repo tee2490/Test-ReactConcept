f77d13f9a1c123da30db0eedb905f3697d51a05a
Teeradet <tee2490@hotmail.com>
2024-07-13 11:18:20 +0700

    app04-event onclick

--- app04-hooks/src/components/ProductList.jsx
+++ app04-hooks/src/components/ProductList.jsx
@@ -3,25 +3,44 @@ import "./ProductList.css";
 
 function ProductList() {
   const [Products, setProducts] = useState();
+  const [Count, setCount] = useState(0);
+  const [Url, setUrl] = useState("http://localhost:3000/products/");
 
   useEffect(() => {
-    fetch("http://localhost:3000/products/")
+    fetch(Url)
       .then((response) => response.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        setProducts(data);
+        setCount(data.length);
+      })
       .catch((e) => console.log("Failed"));
-  }, []);
+  }, [Url]);
 
   return (
     <div>
+      <div className="product-row">
+        <h2>{Count}</h2>
+        <button onClick={() => setUrl("http://localhost:3000/products/")}>
+          Load All
+        </button>
+        <button onClick={()=>setUrl("http://localhost:3000/products?in_stock=true")}>Load In Stock</button>
+      </div>
+
       {Products &&
         Products.map((item) => (
-          <div className="product-list">
+          <div key={item.id} className="product-list">
             <div className="product-card">
               <div className="product-id">{item.id}</div>
-              <div className="product-title">{item.name.length > 30 ? item.name.slice(0,30)+"...":item.name}</div>
+              <div className="product-title">
+                {item.name.length > 30
+                  ? item.name.slice(0, 30) + "..."
+                  : item.name}
+              </div>
               <div className="product-row">
                 <div className="product-price">{item.price}</div>
-                <div className="product-stock">{item.in_stock ? "In Stock" : "Unavailable"}</div>
+                <div className="product-stock">
+                  {item.in_stock=="true" ? "In Stock" : "Unavailable"}
+                </div>
               </div>
             </div>
           </div>
