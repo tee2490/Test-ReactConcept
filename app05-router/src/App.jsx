bd7b94af9f37ddcdd392d42f7760bd0bc217a854
Teeradet <tee2490@hotmail.com>
2024-07-27 08:55:08 +0700

    app06-Create Project

--- app05-router/src/App.jsx
+++ app05-router/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "./components";
 import MainRoute from "./components/MainRoute";
+import MuiHeader from "./components/MuiHeader";
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <MuiHeader/>
       <MainRoute/>
     </div>
   );
